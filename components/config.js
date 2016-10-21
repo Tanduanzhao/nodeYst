ac1592c2733c1dee34c0aef6d1ecddfcd4b8fbd9
cashely.shi <290119516@qq.com>
2016-10-21 23:33:59 +0800

    2016-10-21 v02

--- components/config.js
+++ components/config.js
@@ -3,5 +3,5 @@
 //export const httpAddress = 'http://192.168.1.233:8021/modm/business/';
 export const httpAddress = 'http://yst.immortalshealth.com/modm/';
 
-export const WXKEY = 'wx5b09933bb205ed95';
+export const WXKEY = 'wx06af7b346a9e1f77';
 export const HTTPURL = 'http://yst.immortalshealth.com';
