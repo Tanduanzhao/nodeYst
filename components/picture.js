c8b17e60b849a8f5aa96f119203d6679e42dec2a
cashely.shi <290119516@qq.com>
2016-10-21 23:53:35 +0800

    2016-10-21 11

--- components/picture.js
+++ components/picture.js
@@ -9,8 +9,8 @@ export default class Picture extends Component{
     render(){
         return(
             <div className="root">
-                <div style={{width:'100%',height:'100%',overflowScrolling:'touch',overFlow:'auto'}}>
-                    <iframe ref='frame' style={{display:'block',overFlow:'auto'}} width="100%" height="100%" name="iFrame1" width="100%" scrolling="yes" src={this.state.url}>
+                <div  className="scroll-iframe" style={{width:'100%',height:'100%',overflowScrolling:'touch',overFlow:'auto'}}>
+                    <iframe ref='frame' style={{display:'flex',overFlow:'auto'}} width="100%" height="100%" name="iFrame1" width="100%" scrolling="yes" src={this.state.url}>
 
                     </iframe>
                 </div>
